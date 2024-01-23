c51fc3c29bc3c30ce576585f14f3bc72dee7f7bc
schnusch <schnusch@users.noreply.github.com>
2024-01-23 17:57:09 +0100

    fix copyright year

--- splendid_invoice/mail.py
+++ splendid_invoice/mail.py
@@ -1,6 +1,6 @@
 """
 splendid-invoice
-Copyright (C) 2022-2023  schnusch
+Copyright (C) 2022-2024  schnusch
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
